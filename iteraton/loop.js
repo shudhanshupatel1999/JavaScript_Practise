2f536c69b6e013ea4f3b465fe0513c4227f184c9
Shudhanshu Patel <33798196+shudhanshupatel1999@users.noreply.github.com>
2023-08-27 19:11:15 +0000

    loop

--- iteraton/loop.js
+++ iteraton/loop.js
@@ -9,8 +9,23 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const newNums = myNums.filter( (num) => {
-    num > 4;
-})
+//Direct return if no scope{} is open
+
+// const newNums2 = myNums.filter( (num) => num > 4
+// )
+
+//Explicit return because {} is open
+// const newNums = myNums.filter( (num) => {
+//    return num > 4;
+// })
+
+const newNums = []
+
+myNums.forEach( (num) => {
+    if(num > 3){
+        newNums.push(num);
+    }
+} )
+
 
 console.log(newNums);
