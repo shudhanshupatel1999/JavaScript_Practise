39e7379f9d1144ffff187e461c749b57db220890
Shudhanshu Patel <33798196+shudhanshupatel1999@users.noreply.github.com>
2023-08-27 18:13:42 +0000

    Rename

---
+++ iteraton/Forin.js
@@ -0,0 +1,30 @@
+//Forin loop should only be used for the object iteraton
+
+// const myObject = {
+//     js: 'JavaScript',
+//     cpp: 'C++',
+//     rb: 'Ruby',
+//     swift: 'Swift by apple'
+// }
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programmig = ["js", "rb", "py", "cpp"];
+
+// for (const key in programmig) {
+//     console.log(programmig[key]);
+// }
+
+//For in loop will not work on for Maps
+
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('USA', "United States of America");
+// map.set('Fr', "Frace");
+// map.set('IN', "India");
+
+// for (const key in map) {
+//     console.log(key);
+// }
---
+++ iteraton/Forof.js
@@ -0,0 +1,36 @@
+// //for of
+
+// const arr = [1, 2, 3, 4, 5];
+
+// for (const nums of arr) {
+//     console.log(nums);
+// }
+
+// const gretting = "Hello World!";
+// for (const greet of gretting) {
+//     console.log(`Each charater is : ${greet}`);
+// }
+
+//Maps
+
+const map = new Map();
+map.set('IN', "India");
+map.set('USA', "United States of America");
+map.set('Fr', "Frace");
+map.set('IN', "India");
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObject = {
+    'game1': 'NFS',
+    'game2': 'Spiderman'
+}
+
+//Not Work
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
